15733ac94e9f1af9861c795e04e6bd31ec6a6da1
billies9 <49171439+billies9@users.noreply.github.com>
2019-04-02 20:54:10 -0400

    Built graph class

--- graph.py
+++ graph.py
@@ -20,6 +20,9 @@ from bokeh.models.tools import HoverTool
 from portfolio_page_construction import construct_portfolio, covariance_matrix
 from bokeh.palettes import Spectral6
 from bokeh.transform import linear_cmap
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 # Alpha_Vantage_API = '2IJFMFJU2M5IDU6N'
 Tradier_API = 'Bearer qLUA59t6iQGIgASUpKY9AstSAiNC'
@@ -55,12 +58,13 @@ class Security_Portfolio_data():
     def create_connection(self):
         connection = client.HTTPSConnection('proxy.server', 3128, timeout= 30)
         connection.set_tunnel('sandbox.tradier.com', 443) # need to add proxy.server:3128 here
+        # connection = client.HTTPSConnection('sandbox.tradier.com', 443, timeout=30)
         headers = {'Accept':'application/json'}
         Tradier_API = 'Bearer qLUA59t6iQGIgASUpKY9AstSAiNC'
         headers['Authorization'] = Tradier_API
         return connection, headers
 
-    def type_security_content(self, frame_title, date_type = None):
+    def type_security_content(self, frame_title, date_type = 'hist'):
         if date_type == 'hist':
             if frame_title == 'SPX': frame_title = 'SPY'
             elif frame_title == 'DJIA': frame_title = 'DIA'
@@ -91,7 +95,19 @@ class Security_Portfolio_data():
     def load_security_content(self, connection):
         response = connection.getresponse()
         content = response.read().decode("utf-8")
-        data = json.loads(content)
+        try:
+            data = json.loads(content)
+            print('here')
+        except:
+            _ = json.dumps(content)
+            data = json.loads(_)
+            print('there')
+        # try:
+        #     content = response.read().decode("utf-8")
+        #     print(type(content))
+        #     data = json.loads(content)
+        # except:
+        #     data = json.loads(response.read())
         return data
 
     def portfolio_content(self, weights = None):
@@ -114,69 +130,50 @@ class Security_Portfolio_data():
                 dfreturns.loc[0, ticker + ' Weighted Return'] = _ * float(weights[ticker]) # Weighted
         return dfclose, dfreturns
 
-# def tradier_content(frame_title, dates, date_type = 'hist', weights = None):
-#     connection = client.HTTPSConnection('sandbox.tradier.com', 443, timeout=30)
-#     headers={'Accept':'application/json'}
-#     headers['Authorization'] = Tradier_API
-#     if frame_title == 'Portfolio':
-#         # Create single dataframe based on weights inputs
-#         if weights == None:
-#             print('error - no weights given')
-#         dfclose = pd.DataFrame(columns = [ticker + ' close' for ticker in weights.keys() if weights[ticker] != ''])
-#         dfreturns = pd.DataFrame({ticker + ' Weighted Return':[0] for ticker in weights.keys() if weights[ticker] != ''})
-#
-#         i = 0
-#         for column in dfclose.columns:
-#             ticker = column.split(' ')[0]
-#             connection.request('GET', '/v1/markets/history?symbol={}&start={}&end={}'.format(ticker, dates[0], dates[1]), None, headers)
-#             response = connection.getresponse()
-#             content = response.read().decode("utf-8")
-#             data = json.loads(content)
-#             frame = pd.DataFrame(data['history']['day'])
-#             if i != 0:
-#                 frame.set_index(pd.to_datetime(frame['date']), inplace=True)
-#             dfclose[ticker + ' close'] = frame['close']
-#             dfclose[ticker + ' returns'] = frame['close'].pct_change() * 100
-#             if not isinstance(dfclose.index, pd.DatetimeIndex):
-#                 dfclose.set_index(pd.to_datetime(frame['date']), inplace = True)
-#             try:
-#                 _ = (dfclose.loc[dates[0], ticker + ' close'] - dfclose.loc[dates[1], ticker + ' close']) / dfclose.loc[dates[1], ticker + ' close']
-#                 dfreturns.loc[0, ticker + ' Weighted Return'] = _ * float(weights[ticker])
-#             except:
-#                 dates[1] = pd.to_datetime(dates[1])-timedelta(1)
-#                 _ = (dfclose.loc[dates[0], ticker + ' close'] - dfclose.loc[dates[1], ticker + ' close']) / dfclose.loc[dates[1], ticker + ' close']
-#                 dfreturns.loc[0, ticker + ' Weighted Return'] = _ * float(weights[ticker])
-#             i += 1
-#         return dfclose, dfreturns
-#
-#     if date_type=='intra':
-#         # interval of tick, 1min, 5min, 15min (default)
-#         connection.request('GET', '/v1/markets/timesales?symbol={}&interval=1min&start={}&end={}&session_filter={}'.format(frame_title, dates[0], dates[1], 'open'))
-#         # Returns dictionary of dictionary of dictionary - keys: 'series', 'data'
-#     else:
-#         connection.request('GET', '/v1/markets/history?symbol={}&start={}&end={}'.format(frame_title, dates[0], dates[1]), None, headers) # default interval is daily
-#     # Returns dictionary of dictionary of dictionary - keys: 'history', 'day'
-#     response = connection.getresponse()
-#     content = response.read().decode("utf-8")
-#     data = json.loads(content)
-#     try:
-#         df = pd.DataFrame(data['history']['day'])
-#         return df.set_index('date')
-#     except:
-#         try:
-#             df = pd.DataFrame(data['series']['data'])
-#             return df.set_index('time')
-#         except:
-#             print('Error in retrieving information for ' + frame_title)
-#             return
+class Build_graph():
+    def __init__(self, ticker, dates):
+        self.dates = dates
+        self.ticker = ticker
+        if self.ticker =='SPX':
+            self.ticker = 'SPY'
+            self.multiplier = 10
+        elif self.ticker == 'DJIA':
+            self.ticker = 'DIA'
+            self.multiplier = 100
+        else:
+            self.multiplier = 1
 
+    def price_graph(self, date_type):
+        data = Security_Portfolio_data(self.dates).type_security_content(self.ticker, date_type)
+        data['close'] = data['close'] * self.multiplier
 
-def monthdelta(date, delta):
-    m, y = (date.month+delta) % 12, date.year + ((date.month)+delta-1) // 12
-    if not m: m = 12
-    d = min(date.day, [31,
-        29 if y%4==0 and not y%400==0 else 28,31,30,31,30,31,31,30,31,30,31][m-1])
-    return date.replace(day=d,month=m, year=y)
+        trace = go.Scatter(
+                x = data.index,
+                y = data['close'],
+                mode = 'lines'
+        )
+        end_data = [trace]
+        graph = json.dumps(end_data, cls=plotly.utils.PlotlyJSONEncoder)
+        return graph
+
+    def regression_graph(self, date_type):
+        data = Security_Portfolio_data(self.dates).type_security_content(self.ticker, date_type)
+        data['Percent Change'] = data['close'].pct_change() * 100
+        # Use SPY as proxy for SPX
+        spx_data = Security_Portfolio_data(dates).type_security_content('SPY', date_type)
+        data['SPX Pct Change'] = (spx_data['close'] * 10).pct_change() * 100
+        data.dropna(inplace=True)
+
+        trace = go.Scatter(
+                x = data['SPX Pct Change'],
+                y = data['Percent Change']
+        )
+        end_data = [trace]
+        graph = json.dumps(end_data, cls=plotly.utils.PlotlyJSONEnconder)
+        return graph
+
+    def portfolio_graph(self, weights):
+        return
 
 def build_interactive_graph(frame_title, type = None, weights = None, dates = None, date_type = 'hist'):
     """ Type: 'P' is Price graphing;
@@ -282,85 +279,140 @@ def build_interactive_graph(frame_title, type = None, weights = None, dates = No
 
 
 #--------------------------------------------------------------------------------------------------------------------------
-def calc_return_type(name, dates, return_type = 'D'):
-    # Aggregate based on choice of Daily, Monthly, Yearly
-    df_copy = dfs[name].copy().set_index('DateTime', inplace=True).loc[pd.to_datetime(dates[0]):pd.to_datetime(dates[1]), :] # Between dates and all columns
-    print(df_copy)
-    entry_len = len(df_copy)
-
-    entry_arr = np.zeros((entry_len, 1))
-    for i in range(entry_arr.shape[0]):
-        entry[i][0] = 1 + df_copy.iloc[i]['Percent Change'] / 100
-    df['1+r'] = entry
-
-    if return_type == 'D':
-        return df_copy
-    elif return_type == 'M':
-        for date in df.index:
-            pass
-
-        return
-    else: #return_type=='Y'
-        pass
-    return
-
-def build_static_graph(frame_title):
-    img = io.BytesIO()
-    dfs[[name for name, ticker in mapping.items() if ticker == frame_title][0]].plot(y = 'Price')
-    plt.savefig(img, format='png', dpi= 100, bbox_inches='tight')
-    img.seek(0)
-    graph_url = base64.b64encode(img.getvalue()).decode()
-    plt.close()
-    return 'data:image/png;base64,{}'.format(graph_url)
-
-def regression(frame_title):
-    img = io.BytesIO()
-    fig, ax = plt.subplots()
-    x = dfs['S&P 500 Index']['Percent Change']
-    y = dfs[[name for name, ticker in mapping.items() if ticker == frame_title][0]]['Percent Change']
-    ax.scatter(x, y)
-    fit_func = np.poly1d(np.polyfit(x, y, deg = 1))
-    (ax.plot(x, fit_func(x), 'r', label = "Beta: " + str(round(fit_func[1], 3)) +
-                                            '\n' + "Alpha: " + str(round(fit_func[0], 3))))
-    ax.get_xaxis().get_label().set_visible(True)
-    ax.set_title("Regression")
-    ax.axhline(0, linestyle = '--', color = 'k', linewidth = .7)
-    ax.axvline(0, linestyle = '--', color = 'k', linewidth = .7)
-    ax.legend(loc = 0)
-
-    plt.savefig(img,  format='png', dpi= 100, bbox_inches='tight')
-    img.seek(0)
-    graph_url = base64.b64encode(img.getvalue()).decode()
-    plt.close()
-    return 'data:image/png;base64,{}'.format(graph_url)
-
-class YahooFinanceHistory:
-    timeout = 2
-    crumb_link = 'https://finance.yahoo.com/quote/{0}/history?p={0}'
-    crumble_regex = r'CrumbStore":{"crumb":"(.*?)"}'
-    quote_link = 'https://query1.finance.yahoo.com/v7/finance/download/{quote}?period1={dfrom}&period2={dto}&interval=1d&events=history&crumb={crumb}'
-
-    def __init__(self, symbol, days_back=7):
-        self.symbol = symbol
-        self.session = requests.Session()
-        self.dt = timedelta(days=days_back)
-
-    def get_crumb(self):
-        response = self.session.get(self.crumb_link.format(self.symbol), timeout=self.timeout)
-        response.raise_for_status()
-        match = re.search(self.crumble_regex, response.text)
-        if not match:
-            raise ValueError('Could not get crumb from Yahoo Finance')
-        else:
-            self.crumb = match.group(1)
-
-    def get_quote(self):
-        if not hasattr(self, 'crumb') or len(self.session.cookies) == 0:
-            self.get_crumb()
-        now = datetime.utcnow()
-        dateto = int(now.timestamp())
-        datefrom = int((now - self.dt).timestamp())
-        url = self.quote_link.format(quote=self.symbol, dfrom=datefrom, dto=dateto, crumb=self.crumb)
-        response = self.session.get(url)
-        response.raise_for_status()
-        return pd.read_csv(StringIO(response.text), parse_dates=['Date'])
+# def tradier_content(frame_title, dates, date_type = 'hist', weights = None):
+#     connection = client.HTTPSConnection('sandbox.tradier.com', 443, timeout=30)
+#     headers={'Accept':'application/json'}
+#     headers['Authorization'] = Tradier_API
+#     if frame_title == 'Portfolio':
+#         # Create single dataframe based on weights inputs
+#         if weights == None:
+#             print('error - no weights given')
+#         dfclose = pd.DataFrame(columns = [ticker + ' close' for ticker in weights.keys() if weights[ticker] != ''])
+#         dfreturns = pd.DataFrame({ticker + ' Weighted Return':[0] for ticker in weights.keys() if weights[ticker] != ''})
+#
+#         i = 0
+#         for column in dfclose.columns:
+#             ticker = column.split(' ')[0]
+#             connection.request('GET', '/v1/markets/history?symbol={}&start={}&end={}'.format(ticker, dates[0], dates[1]), None, headers)
+#             response = connection.getresponse()
+#             content = response.read().decode("utf-8")
+#             data = json.loads(content)
+#             frame = pd.DataFrame(data['history']['day'])
+#             if i != 0:
+#                 frame.set_index(pd.to_datetime(frame['date']), inplace=True)
+#             dfclose[ticker + ' close'] = frame['close']
+#             dfclose[ticker + ' returns'] = frame['close'].pct_change() * 100
+#             if not isinstance(dfclose.index, pd.DatetimeIndex):
+#                 dfclose.set_index(pd.to_datetime(frame['date']), inplace = True)
+#             try:
+#                 _ = (dfclose.loc[dates[0], ticker + ' close'] - dfclose.loc[dates[1], ticker + ' close']) / dfclose.loc[dates[1], ticker + ' close']
+#                 dfreturns.loc[0, ticker + ' Weighted Return'] = _ * float(weights[ticker])
+#             except:
+#                 dates[1] = pd.to_datetime(dates[1])-timedelta(1)
+#                 _ = (dfclose.loc[dates[0], ticker + ' close'] - dfclose.loc[dates[1], ticker + ' close']) / dfclose.loc[dates[1], ticker + ' close']
+#                 dfreturns.loc[0, ticker + ' Weighted Return'] = _ * float(weights[ticker])
+#             i += 1
+#         return dfclose, dfreturns
+#
+#     if date_type=='intra':
+#         # interval of tick, 1min, 5min, 15min (default)
+#         connection.request('GET', '/v1/markets/timesales?symbol={}&interval=1min&start={}&end={}&session_filter={}'.format(frame_title, dates[0], dates[1], 'open'))
+#         # Returns dictionary of dictionary of dictionary - keys: 'series', 'data'
+#     else:
+#         connection.request('GET', '/v1/markets/history?symbol={}&start={}&end={}'.format(frame_title, dates[0], dates[1]), None, headers) # default interval is daily
+#     # Returns dictionary of dictionary of dictionary - keys: 'history', 'day'
+#     response = connection.getresponse()
+#     content = response.read().decode("utf-8")
+#     data = json.loads(content)
+#     try:
+#         df = pd.DataFrame(data['history']['day'])
+#         return df.set_index('date')
+#     except:
+#         try:
+#             df = pd.DataFrame(data['series']['data'])
+#             return df.set_index('time')
+#         except:
+#             print('Error in retrieving information for ' + frame_title)
+#             return
+# def calc_return_type(name, dates, return_type = 'D'):
+#     # Aggregate based on choice of Daily, Monthly, Yearly
+#     df_copy = dfs[name].copy().set_index('DateTime', inplace=True).loc[pd.to_datetime(dates[0]):pd.to_datetime(dates[1]), :] # Between dates and all columns
+#     print(df_copy)
+#     entry_len = len(df_copy)
+#
+#     entry_arr = np.zeros((entry_len, 1))
+#     for i in range(entry_arr.shape[0]):
+#         entry[i][0] = 1 + df_copy.iloc[i]['Percent Change'] / 100
+#     df['1+r'] = entry
+#
+#     if return_type == 'D':
+#         return df_copy
+#     elif return_type == 'M':
+#         for date in df.index:
+#             pass
+#
+#         return
+#     else: #return_type=='Y'
+#         pass
+#     return
+#
+# def build_static_graph(frame_title):
+#     img = io.BytesIO()
+#     dfs[[name for name, ticker in mapping.items() if ticker == frame_title][0]].plot(y = 'Price')
+#     plt.savefig(img, format='png', dpi= 100, bbox_inches='tight')
+#     img.seek(0)
+#     graph_url = base64.b64encode(img.getvalue()).decode()
+#     plt.close()
+#     return 'data:image/png;base64,{}'.format(graph_url)
+#
+# def regression(frame_title):
+#     img = io.BytesIO()
+#     fig, ax = plt.subplots()
+#     x = dfs['S&P 500 Index']['Percent Change']
+#     y = dfs[[name for name, ticker in mapping.items() if ticker == frame_title][0]]['Percent Change']
+#     ax.scatter(x, y)
+#     fit_func = np.poly1d(np.polyfit(x, y, deg = 1))
+#     (ax.plot(x, fit_func(x), 'r', label = "Beta: " + str(round(fit_func[1], 3)) +
+#                                             '\n' + "Alpha: " + str(round(fit_func[0], 3))))
+#     ax.get_xaxis().get_label().set_visible(True)
+#     ax.set_title("Regression")
+#     ax.axhline(0, linestyle = '--', color = 'k', linewidth = .7)
+#     ax.axvline(0, linestyle = '--', color = 'k', linewidth = .7)
+#     ax.legend(loc = 0)
+#
+#     plt.savefig(img,  format='png', dpi= 100, bbox_inches='tight')
+#     img.seek(0)
+#     graph_url = base64.b64encode(img.getvalue()).decode()
+#     plt.close()
+#     return 'data:image/png;base64,{}'.format(graph_url)
+#
+# class YahooFinanceHistory:
+#     timeout = 2
+#     crumb_link = 'https://finance.yahoo.com/quote/{0}/history?p={0}'
+#     crumble_regex = r'CrumbStore":{"crumb":"(.*?)"}'
+#     quote_link = 'https://query1.finance.yahoo.com/v7/finance/download/{quote}?period1={dfrom}&period2={dto}&interval=1d&events=history&crumb={crumb}'
+#
+#     def __init__(self, symbol, days_back=7):
+#         self.symbol = symbol
+#         self.session = requests.Session()
+#         self.dt = timedelta(days=days_back)
+#
+#     def get_crumb(self):
+#         response = self.session.get(self.crumb_link.format(self.symbol), timeout=self.timeout)
+#         response.raise_for_status()
+#         match = re.search(self.crumble_regex, response.text)
+#         if not match:
+#             raise ValueError('Could not get crumb from Yahoo Finance')
+#         else:
+#             self.crumb = match.group(1)
+#
+#     def get_quote(self):
+#         if not hasattr(self, 'crumb') or len(self.session.cookies) == 0:
+#             self.get_crumb()
+#         now = datetime.utcnow()
+#         dateto = int(now.timestamp())
+#         datefrom = int((now - self.dt).timestamp())
+#         url = self.quote_link.format(quote=self.symbol, dfrom=datefrom, dto=dateto, crumb=self.crumb)
+#         response = self.session.get(url)
+#         response.raise_for_status()
+#         return pd.read_csv(StringIO(response.text), parse_dates=['Date'])
