736f51556bfe030cc54937ee3d9363730af1783c
billies9 <49171439+billies9@users.noreply.github.com>
2019-05-24 10:00:06 -0400

    Depecate file to normal functionality
    Includes only plot resizing

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,14 +1,31 @@
 function plot(plot_type, id) {
 
-  window.addEventListener('resize', func);
-  
+  window.addEventListener('resize', plot_resize);
+  // document.getElementById('demo').innerHTML = 'Data: <br>' + plot_type.points;
   Plotly.newPlot(id, plot_type, {}, {responsive:true});
-  function func() {
+
+  function plot_resize() {
     var width = document.getElementById(id).clientWidth ;
+    // document.getElementById("demo").innerHTML = 'Width: ' + width;
+    // var update = {
+    //   title: 'Total Width: ' + width,
+    //   width: width,
+    //   height: 0.9 * window.innerHeight
+    //
+    // }
     Plotly.relayout(id, {
       width: width,
       height: 0.9 * window.innerHeight
     })
+    // Plotly.relayout(id, update)
   };
+
+  // id.on('plotly_hover', plot(plot_type, id) {
+  //     var title_change = 'Title Change'
+  //     Plotly.relayout(id, {
+  //       title: title_change
+  //     })
+  //   });
+
 
 }
