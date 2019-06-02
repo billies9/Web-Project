55e94c4f50a0e488edd6301cfa8aadd59033f37a
billies9 <49171439+billies9@users.noreply.github.com>
2019-06-02 13:16:43 -0400

    Portfolio Value at Risk calculations

--- static/py/portfolio_stats.py
+++ static/py/portfolio_stats.py
@@ -20,3 +20,18 @@ class Portfolio_Stats():
             slope, intercept, r_value, p_value, std_err = stats.linregress(self.SPX_data, y_data)
             self.tickers_data.drop('Portfolio Return', axis=1, inplace=True)
         return slope, intercept, r_value**2 # Beta, Alpha, R-squared
+
+    def VaR(self, weights):
+        self.tickers_data.loc[:, 'Portfolio Return'] = np.dot(weights, self.tickers_data.T)
+        mu = self.tickers_data['Portfolio Return'].mean()
+        sigma = self.tickers_data['Portfolio Return'].std()
+
+        self.tickers_data.drop('Portfolio Return', axis=1, inplace=True)
+        P_value = 1e6
+
+        VaR = []
+        for c_level in [.90, .95, .975, .99]:
+            alpha = stats.norm.ppf(1 - c_level, mu, sigma)
+            VaR.append(P_value - P_value*(alpha + 1))
+        return VaR
+            # self.tickers_data.loc[:, "{c_level}% Confidence Level VaR".format(c_level = c_level * 100)]
